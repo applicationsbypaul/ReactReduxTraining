a0732dfb7e6334ec2d7ba2266025fbcd6251f29c
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2022-02-17 11:32:11 -0600

    plugins and modules

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpack = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 process.env.NODE_ENV = 'development';
 
@@ -21,5 +21,24 @@ module.exports = {
         disableHostCheck: true,
         headers: { "Access-Control_Allow_Origin": "*"},
         https: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            favicon: "src/favicon.ico"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /(\.css)$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
     }
-}
+};
